1e5bac677909f433f77b9ab288dce65d0e1b8400
Mykhailo <voloshenkomykhailo@gmail.com>
2025-05-17 00:01:19 +0300

    constructor added, request json decoded

--- internal/handlers/subscription_handler.go
+++ internal/handlers/subscription_handler.go
@@ -1,23 +1,49 @@
 package handlers
 
-import "net/http"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"weather-api/internal/models"
+)
 
-type SubscriptionHandler struct{}
-
-func (sh *SubscriptionHandler) PostSubscription(writer http.ResponseWriter, r *http.Request) {
-	// Simulate processing subscription
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("Subscription successful"))
+type SubscriptionHandler struct {
+	conn *sql.DB
 }
 
-func (sh *SubscriptionHandler) GetConfirm(writer http.ResponseWriter, r *http.Request) {
+func NewSubscriptionHandler(conn *sql.DB) *SubscriptionHandler {
+	return &SubscriptionHandler{
+		conn: conn,
+	}
+}
+
+func (sh *SubscriptionHandler) PostSubscription(rw http.ResponseWriter, r *http.Request) {
+	var subscription models.Subscription
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&subscription); err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+	email := subscription.Email
+	city := subscription.City
+	frequency := subscription.Frequency
+
+	rw.Header().Set("Content-Type", "application/json")
+
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(fmt.Sprintf("%s, %s, %s", email, city, frequency)))
+}
+
+func (sh *SubscriptionHandler) GetConfirm(rw http.ResponseWriter, r *http.Request) {
 	// Simulate confirming subscription
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("Subscription confirmed"))
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("Subscription confirmed"))
 }
 
-func (sh *SubscriptionHandler) GetUnsubscribe(writer http.ResponseWriter, r *http.Request) {
+func (sh *SubscriptionHandler) GetUnsubscribe(rw http.ResponseWriter, r *http.Request) {
 	// Simulate unsubscribing
-	writer.WriteHeader(http.StatusOK)
-	writer.Write([]byte("Unsubscribed successfully"))
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("Unsubscribed successfully"))
 }
