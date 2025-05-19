01483be3d6d61fd39adff820f072647d457292d3
voloshmiak <voloshenkomykhailo@gmail.com>
2025-05-19 20:46:51 +0300

    clear the entry point file

--- cmd/weather-api/main.go
+++ cmd/weather-api/main.go
@@ -1,65 +1,105 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/jackc/pgx/v5/stdlib"
-	"github.com/joho/godotenv"
+	"log"
 	"net/http"
-	"os"
-	"weather-api/internal/handlers"
+	"os/signal"
+	"syscall"
+	"time"
+	"weather-api/internal/config"
+	"weather-api/internal/handler"
 	"weather-api/internal/repository"
 	"weather-api/internal/service"
 )
 
 func main() {
-	err := godotenv.Load()
+	// get the database URL from environment variables
+	migrationURL := config.GetMigrationURL()
+	dbURL := config.GetDatabaseURL()
+
+	log.Printf("Trying to run migrations from: %s", migrationURL)
+
+	// run the migrations
+	m, err := migrate.New(migrationURL, dbURL)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to initialize migrations: %v. Migration URL: %s, DB URL: %s", err, migrationURL, dbURL)
 	}
 
-	var (
-		user     = os.Getenv("DB_USER")
-		password = os.Getenv("DB_PASSWORD")
-		url      = fmt.Sprintf("host=localhost port=5432 dbname=weather-database user=%s password=%s", user, password)
-	)
-
-	conn, err := sql.Open("pgx", url)
-	if err != nil {
-		panic(err)
+	if err := m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatalf("Failed to apply migrations: %v", err)
+	} else if errors.Is(err, migrate.ErrNoChange) {
+		log.Println("Migrations: No new migrations to apply.")
+	} else {
+		log.Println("Migrations applied successfully!")
 	}
 
-	// init mux
+	// connect to the database
+	log.Printf("Trying to connect to database: " + dbURL)
+
+	conn, err := repository.NewPostgresDB()
+	if err != nil {
+		log.Fatalf("FATAL: Failed to connect to database: %v", err)
+	}
+
+	log.Println("Successfully connected to Database!")
+
+	// set up the HTTP server
 	mux := http.NewServeMux()
+	repos := repository.NewRepository(conn)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+	handlers.RegisterRoutes(mux)
 
-	// init repo
-	subscriptionRepo := repository.NewSubscriptionRepository(conn)
-
-	// init service
-	subscriptionService := service.NewSubscriptionService(subscriptionRepo)
-
-	// init handlers
-	weatherHandler := new(handlers.WeatherHandler)
-	subscriptionHandler := handlers.NewSubscriptionHandler(subscriptionService)
-
-	// weather
-	mux.HandleFunc("GET /weather", weatherHandler.GetWeather)
-
-	// subscription
-	mux.HandleFunc("POST /subscribe", subscriptionHandler.PostSubscription)
-	mux.HandleFunc("GET /confirm/{token}", subscriptionHandler.GetConfirm)
-	mux.HandleFunc("GET /unsubscribe/{token}", subscriptionHandler.GetUnsubscribe)
-
-	mux.Handle("/api/", http.StripPrefix("/api", mux))
-
-	// server configuration
+	apiPort := config.GetAPIPort()
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + apiPort,
 		Handler: mux,
 	}
 
+	// Set up graceful shutdown
+	done := make(chan bool, 1)
+
+	go gracefulShutdown(done, server, conn)
+
 	// start listening
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
+	log.Printf("Starting server on port %s", apiPort)
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatalf("Failed to start server: %v", err)
 	}
+
+	// wait for shutdown signal
+	<-done
+	log.Println("Server shutdown complete")
+}
+
+func gracefulShutdown(done chan bool, server *http.Server, conn *sql.DB) {
+	// wait for interruption
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	<-ctx.Done()
+
+	log.Println("Shutting down server gracefully")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Println(fmt.Sprintf("Server forced to shutdown: %s", err))
+	}
+
+	err := conn.Close()
+	if err != nil {
+		log.Println(fmt.Sprintf("Failed to close database: %s", err))
+	}
+
+	done <- true
 }
